14bb92ba23acfc68500b36e915a7a49f4681e998
luyifo <luyifo@163.com>
2024-06-12 00:19:16 +0800

    nothing

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,7 @@
+import { WebSocket } from "ws";
+import got from "got";
+
+const webSocket = new WebSocket("");
 class DingTalkCredential {
     clientId: string;
     clientSecret: string;
@@ -8,20 +12,64 @@ class DingTalkCredential {
     }
 }
 
-interface IEventHandler {
-    process(): never;
+interface IEventListener {
+    onEvent(): never;
 }
+
+type SubscriptionType = "SYSTEM" | "EVENT" | "CALLBACK";
+
+type Topics = {
+    SYSTEM: "ping" | "disconnect";
+    EVENT: "*";
+    CALLBACK: "robot" | "card";
+};
+
+
+class Subscription {
+    readonly type: SubscriptionType;
+    readonly topic: string;
+
+    private constructor(type: SubscriptionType, topic: string) {
+        this.type = type;
+        this.topic = topic;
+    }
+
+    static create<T extends SubscriptionType>(type: T, topic: Topics[T]) {
+        return new Subscription(type, topic);
+    }
+}
+
 
 
 class StreamClient {
     credential: DingTalkCredential;
+    subscriptions: Subscription[];
 
     constructor(credential: DingTalkCredential) {
         this.credential = credential;
+        this.subscriptions = [];
+
+        const pingSubscription = Subscription.create("SYSTEM", "ping");
+        const disconnectSubscription = Subscription.create("SYSTEM", "disconnect");
+
+        this.subscriptions.push(pingSubscription);
+        this.subscriptions.push(disconnectSubscription);
     }
 
-    registerAllEvent(callback: IEventHandler) {
-        
+    registerAllEvent(listener: IEventListener) {
+        const subscription = Subscription.create("EVENT", "*");
+        this.subscriptions.push(subscription);
+    }
+
+    async connect() {
+        const url = "https://api.dingtalk.com/v1.0/gateway/connections/open";
+
+        const body = {
+            ...this.credential,
+            subscriptions: this.subscriptions,
+        };
+        const { data } = await got.post(url, { json: body }).json();
+        console.log('data ==> ', data);
     }
 }
 
